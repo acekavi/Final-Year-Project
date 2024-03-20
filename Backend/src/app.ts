3dca8c639c8e9ef9552f406091bc0c2cf4d31401
acekavi <avishkakavinda@proton.me>
2024-03-20 12:18:52 +0530

    Unity Project Initialization for AR

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -11,7 +11,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 mongoose.connect(process.env.MONGODB_URI!);
 
 mongoose.connection.on('connected', () => {
