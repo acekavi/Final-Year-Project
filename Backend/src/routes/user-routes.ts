4ebbd6c59cd26344b44c9e67438f013deb8d493c
acekavi <avishkakavinda@proton.me>
2024-04-05 11:39:58 +0530

    added a route for achievement add

--- Backend/src/routes/user-routes.ts
+++ Backend/src/routes/user-routes.ts
@@ -10,9 +10,12 @@ router.post('/signin', UserController.signin_user);
 router.get('/:id', CheckAuth, UserController.get_user);
 router.get('/logged/user', CheckAuth, UserController.get_logged_user);
 router.get('/check/auth', CheckAuth, UserController.get_auth_status);
+router.post('/add/achievement', CheckAuth, UserController.add_achievement);
+
 // router.put('/user/update/:id', CheckAuth, UserController.update_user);
 // router.put('/user/update/password/:id', CheckAuth, UserController.update_user_password);
 // router.put('/user/check_unique_email', CheckAuth, UserController.check_unique_email);
+
 
 // router.put('/user/password-reset/:id', CheckAuth, UserController.first_time_password_reset);
 
